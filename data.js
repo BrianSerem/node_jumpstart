64d0b325bb206504888fc1110f774fb8e9106c1f
Brian Serem <brianseremiat@gmail.com>
2019-09-29 13:44:16 +0300

    create(server): added a simple express server -create a crud application for adding, updatinf, viewing and deleting cars

---
+++ data.js
@@ -0,0 +1,22 @@
+const cars = [
+    {   
+        id: 1,
+        name: 'Model-y',
+        brand: 'Tesla',
+        engine: 'Electric'
+    },
+    {   
+        id: 2,
+        name: 'Leaf',
+        brand: 'Nissan',
+        engine: 'Electric'
+    },
+    {   
+        id: 3,
+        name: 'Aventador',
+        brand: 'Lamborghini',
+        engine: 'Fossil'
+    },
+];
+
+module.exports = cars;
